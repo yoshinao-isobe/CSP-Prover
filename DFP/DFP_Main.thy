65ddda0c7629b31e002e38431f7550025c6825dc
Joabe Jesus <joabebezerra@gmail.com>
2021-09-18 18:58:12 -0300

    Update DFP_Main.thy
    Added DFnonTick and DFkev

--- DFP/DFP_Main.thy
+++ DFP/DFP_Main.thy
@@ -1,11 +1,21 @@
            (*-------------------------------------------*
             |                DFP package                |
             |                 June 2005                 |
+            |                                           |
+            |        CSP-Prover on Isabelle2021         |
+            |                 August 2021  (modified)   |
+            |                                           |
             |        Yoshinao Isobe (AIST JAPAN)        |
+            | Joabe Jesus (eComp POLI UPE and CIn UFPE) |
             *-------------------------------------------*)
 
 theory DFP_Main
 imports DFP_Proof_Rule1 DFP_DFtick
+        DFP_law_DFtick
+        DFP_DFnonTick
+        DFP_law_DFnonTick
+        DFP_DFkev
+        DFP_law_DFkev
 begin
 
 (*****************************************************************
